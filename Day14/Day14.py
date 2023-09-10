6d94fc673ec77b55fa27cb3e80254fab456caf08
JoeM10 <JoeMcDaniel1592@gmail.com>
2023-09-10 13:37:24 -0700

    Updated functionality.

--- Day14/Day14.py
+++ Day14/Day14.py
@@ -4,34 +4,18 @@ import art
 import game_data
 import os
 
-def compareA():
-  comparisonA = random.choice(game_data.data)
-
-  name = comparisonA['name']
-  followerCount = comparisonA['follower_count']
-  description = comparisonA['description']
-  country = comparisonA['country']
-
-  print(f"Compare A: {name}, {description}, from {country}.")
-  return followerCount
-
-def compareB():
-  comparisonB = random.choice(game_data.data)
-
-  name = comparisonB['name']
-  followerCount = comparisonB['follower_count']
-  description = comparisonB['description']
-  country = comparisonB['country']
-
-  print(f"Against B: {name}, {description}, from {country}.")
-  return followerCount
+def accountInfo(account):
+  name, description, country = account["name"], account["description"], account["country"]
+  return f"{name}, a {description}, from {country}."
 
 def score(currentScore):
   if currentScore > 0:
     print(f"You're right! Current score: {currentScore}.")
 
-def followerCompare(compareA, compareB, currentScore):
-  correctAnswer = "a" if compareA > compareB else "b"
+def followerCompare(accountA, accountB, currentScore):
+  followersA = accountA["follower_count"]
+  followersB = accountB["follower_count"]
+  correctAnswer = "a" if followersA > followersB else "b"
   
   playerGuess = str(input("Who has more followers? Type 'A' or 'B': ")).lower()
   if playerGuess == correctAnswer:
@@ -45,15 +29,21 @@ def followerCompare(compareA, compareB, currentScore):
 
 def game():
   currentScore = 0
+  accountA = random.choice(game_data.data)
   while True:
+    accountB = random.choice(game_data.data)
+    if accountA == accountB:
+      accountB = random.choice(game_data.data)
+
     os.system('cls')
     print(art.logo)
     score(currentScore)
 
-    compA = compareA()
+    print(f"Compare A: {accountInfo(accountA)}")
     print(art.vs)
-    compB = compareB()
+    print(f"Compare B: {accountInfo(accountB)}")
 
-    currentScore = followerCompare(compA, compB, currentScore)
+    currentScore = followerCompare(accountA, accountB, currentScore)
+    accountA = accountB
 
 game()
