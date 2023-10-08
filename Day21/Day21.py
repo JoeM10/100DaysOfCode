d355aed04de0c9915da6c0c78b7ca72eee4f28ea
JoeM10 <JoeMcDaniel1592@gmail.com>
2023-10-07 22:39:56 -0700

    Completed Snake Game.

---
+++ Day21/Day21.py
@@ -0,0 +1,47 @@
+from turtle import Screen
+from Snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+gameOn = True
+while gameOn:
+  screen.update()
+  time.sleep(0.1)
+  snake.moveSnake()
+
+  # Detect collision with food.
+  if snake.head.distance(food) < 15:
+    food.refresh()
+    snake.extend()
+    scoreboard.increaseScore()
+
+  # Detect collison with wall.
+  if snake.head.xcor() > 295 or snake.head.xcor() < -295 or snake.head.ycor() > 295 or snake.head.ycor() < -295:
+    gameOn = False
+    scoreboard.gameOver()
+  
+  # Detect collison with tail.
+  for square in snake.squares[1:]:
+    if snake.head.distance(square) < 10:
+      gameOn = False
+      scoreboard.gameOver()
+
+# KEEP AT BOTTOM--------
+screen.exitonclick()
---
+++ Day21/scoreboard.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Courier", 20, "normal")
+
+class Scoreboard(Turtle):
+
+  def __init__(self):
+    super().__init__()
+    self.score = 0
+    self.color("white")
+    self.hideturtle()
+    self.penup()
+    self.goto(0, 270)
+    self.updateScoreboard()
+
+  def updateScoreboard(self):
+    self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+  def increaseScore(self):
+    self.score += 1
+    self.clear()
+    self.updateScoreboard()
+  
+  def gameOver(self):
+    self.goto(0, 0)
+    self.write("GAME OVER", align=ALIGNMENT, font=FONT)
