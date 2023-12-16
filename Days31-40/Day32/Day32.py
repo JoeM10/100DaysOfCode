6622f9b4f25f64dfd400c89418e8791b97cdfa83
JoeM10 <JoeMcDaniel1592@gmail.com>
2023-12-15 18:49:36 -0800

    Learned how to send emails through Python.

---
+++ Days31-40/Day32/Day32.py
@@ -0,0 +1,47 @@
+import smtplib
+import datetime as dt
+import os
+import random
+scriptDirectory = os.path.dirname(__file__)
+
+# # DATETIME
+# now = dt.datetime.now()
+# year = now.year
+# month = now.month
+# dayOfWeek = now.weekday()
+# print(now)
+# print(year)
+# day = dt.datetime.now().day
+
+# # SMTP INFORMATION
+gmail = "smtp.gmail.com"
+hotmail = "smtp.live.com"
+yahoo = "smtp.mail.yahoo.com"
+
+myEmail = "johnerr370@gmail.com"
+appPassword = "wdvcujfdcslygzwa"
+
+testEmail = "garrhj2464@outlook.com"
+
+# # SENDING A EMAIL WITH GMAIL
+# with smtplib.SMTP(gmail, port=587) as connection:
+#   connection.starttls()
+#   connection.login(user=myEmail, password=appPassword)
+#   For the subject/body of the email, msg="Subject:THIS IS THE SUBJECT\n\nTHIS IS THE BODY"
+#   connection.sendmail(from_addr=myEmail, to_addrs=testEmail, 
+#                       msg="Subject:Hello\n\nThis is the body of my email.")
+
+# Motivational Quote Project
+with open(f"{scriptDirectory}\\quotes.txt", mode="r") as file:
+  quotes = file.readlines()
+quoteOfTheDay = random.choice(quotes)
+
+currentDateTime = dt.datetime.now()
+dayOfWeek = currentDateTime.weekday()
+
+if dayOfWeek == 4:
+  with smtplib.SMTP(gmail, port=587) as connection:
+    connection.starttls()
+    connection.login(user=myEmail, password=appPassword)
+    connection.sendmail(from_addr=myEmail, to_addrs=testEmail,
+                        msg=f"Subject:Motivational Quote of the Day.\n\n{quoteOfTheDay}")
---
+++ Days31-40/Day32/main.py
@@ -0,0 +1,48 @@
+##################### Extra Hard Starting Project ######################
+
+# 1. Update the birthdays.csv ✅
+
+# 2. Check if today matches a birthday in the birthdays.csv ✅
+
+# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv ✅
+
+# 4. Send the letter generated in step 3 to that person's email address. ✅
+
+import datetime as dt
+import os
+import random
+import smtplib
+import pandas
+scriptDirectory = os.path.dirname(__file__)
+# ---------- EMAIL VARIABLES ---------- #
+gmail = "smtp.gmail.com"
+myEmail = "johnerr370@gmail.com"
+appPassword = "wdvcujfdcslygzwa"
+testEmail = "garrhj2464@outlook.com"
+
+# ---------- DATE/TIME VARIABLES ---------- #
+dayOfMonth = dt.datetime.now().day
+monthOfYear = dt.datetime.now().month
+
+birthdaysFile = pandas.read_csv(f"{scriptDirectory}\\birthdays.csv")
+birthdaysDict = birthdaysFile.to_dict()
+
+# How to find the index of a specific value in a column.
+# print(FILE[FILE['COLUMN'] == VALUE].index.values[0])
+
+index = 0
+
+if dayOfMonth in birthdaysDict["day"].values() and monthOfYear in birthdaysDict["month"].values():
+  index = birthdaysFile[birthdaysFile['month'] == monthOfYear].index.values[0] and birthdaysFile[birthdaysFile['day'] == dayOfMonth].index.values[0]
+  print("Good")
+  replaceName = "[NAME]"
+  bDayName = birthdaysDict["name"][index]
+
+  with open(f"{scriptDirectory}\\letter_templates\\letter_{random.randint(1,3)}.txt") as bDayLetterFile:
+    bDayLetterContents = bDayLetterFile.read().replace(replaceName, bDayName)
+  
+  with smtplib.SMTP(gmail, port=587) as connection:
+    connection.starttls()
+    connection.login(user=myEmail, password=appPassword)
+    connection.sendmail(from_addr=myEmail, to_addrs=testEmail,
+                        msg=f"Subject:Happy Birthday!.\n\n{bDayLetterContents}")
